8a560c3ec9f2e9133d17552179c05757a501a959
Huor1 <huor223@gmail.com>
2023-06-30 11:51:43 +0200

    ddd

--- server.py
+++ server.py
@@ -2,7 +2,14 @@ from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 import os
 @app.route("/")
-def hello_world():
+def Home():
+    return render_template("index.html")
+
+@app.route("/About")
+def About():
     return render_template("about.html")
 
+@app.route("/Login")
+def Login():
+    return render_template("test_login.html")
 
