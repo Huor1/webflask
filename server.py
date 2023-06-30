5b7da0e308ffd37dfa4ce3a52e964696ef515b2d
Huor1 <huor223@gmail.com>
2023-06-30 11:19:52 +0200

    ddd

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
-
+import os
 @app.route("/")
 def hello_world():
     return render_template("about.html")
+
+
