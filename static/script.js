9b23d1cad812a8f64ad950d0e310bc73d349d2a2
Huor1 <huor223@gmail.com>
2023-08-09 18:46:28 +0200

    09-08

--- static/script.js
+++ static/script.js
@@ -10,191 +10,174 @@ $(document).ready(function() {
   var programVersion = " Beta 0.19.3"; // Numer programu
 
   // Funkcja do zmiany tła
-  var imagesUKPL = ["static/zdj1.jpg", "static/zdj2.jpg", "static/zdj3.jpg"];
-  var imagesSPQR = ["static/zdj4.jpg", "static/zdj5.jpg", "static/zdj6.jpg"];
-  var currentImageIndex = 0;
-  var clickedFlag = $("#flag1"); // Domyślnie ustawiona flaga UK
+var imagesUKPL = ["static/zdj1.jpg", "static/zdj2.jpg", "static/zdj3.jpg"];
+var imagesSPQR = ["static/zdj4.jpg", "static/zdj5.jpg", "static/zdj6.jpg"];
+var currentImageIndex = 0;
+var clickedFlag = $("#flag1"); // Domyślnie ustawiona flaga UK
 
-  function changeBackgroundImage() {
-    var images = [];
+function changeBackgroundImage() {
+  var images = [];
 
-    if (clickedFlag.attr("id") === "flag1" || clickedFlag.attr("id") === "flag2") {
-      // Jeśli kliknięto flagę UK lub PL, użyj zdjęć dla UK i PL
-      images = imagesUKPL;
-    } else if (clickedFlag.attr("id") === "flag3") {
-      // Jeśli kliknięto flagę SPQR, użyj zdjęć dla SPQR
-      images = imagesSPQR;
-    }
-
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    var imageUrl = images[currentImageIndex];
-    document.body.style.backgroundImage = "url('" + imageUrl + "')";
+  if (clickedFlag.attr("id") === "flag1" || clickedFlag.attr("id") === "flag2") {
+    // Jeśli kliknięto flagę UK lub PL, użyj zdjęć dla UK i PL
+    images = imagesUKPL;
+  } else if (clickedFlag.attr("id") === "flag3") {
+    // Jeśli kliknięto flagę SPQR, użyj zdjęć dla SPQR
+    images = imagesSPQR;
   }
 
-  function startImageRotation() {
-    setInterval(changeBackgroundImage, 5000); // Zmieniaj obraz co 5 sekund
-  }
-  startImageRotation(); // Rozpoczęcie automatycznej rotacji obrazów
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  var imageUrl = images[currentImageIndex];
+  document.body.style.backgroundImage = "url('" + imageUrl + "')";
+}
 
-  // Wywołaj funkcję startImageRotation() po kliknięciu na flagę
-  $("#flag1, #flag2, #flag3").click(function() {
-    clickedFlag = $(this);
-    changeBackgroundImage(); // Natychmiastowa zmiana obrazu po kliknięciu na flagę
-  });
+function startImageRotation() {
+  setInterval(changeBackgroundImage, 5000); // Zmieniaj obraz co 5 sekund
+}
+startImageRotation(); // Rozpoczęcie automatycznej rotacji obrazów
 
-  console.log("script.js loaded"); // Dodanie console.log()
+// Wywołaj funkcję startImageRotation() po kliknięciu na flagę
+$("#flag1, #flag2, #flag3").click(function() {
+  clickedFlag = $(this);
+  changeBackgroundImage(); // Natychmiastowa zmiana obrazu po kliknięciu na flagę
+});
 
-  // Ukryj wszystkie flagi, oprócz flagi UK
-  $('.flag-item:not(#flag1)').hide();
+console.log("script.js loaded"); // Dodanie console.log()
 
-  // Funkcja do tłumaczenia elementów na podstawie mapy translations
-  function translateElements(language) {
-    var translations = {};
+// Ukryj wszystkie flagi, oprócz flagi UK
+$('.flag-item:not(#flag1)').hide();
 
-    if (language === "pl") {
-      translations = {
-        "Zaloguj": "Zaloguj",
-        'label[for="username"]': "Login:",
-        'label[for="password"]': "Hasło:",
-        ".Dane_uzytkownika": "Dane użytkownika",
-        ".Wersja_programu": "Wersja programu" + programVersion,
-        ".Autorzy": "Autorzy Jakub Kołodziej i Piotr Wieczorek",
-        ".title": "Estymowanie cen nieruchomości",
-        ".pokoje-tekst": "Pokoje:",
-        ".powierzchnia-tekst": "Powierzchnia:",
-        ".prognozowana-cena": "Prognozowana cena",
-        ".active": "Strona główna",
-        ".Funkcja1": "Funkcja 1",
-        ".Funkcja2": "Funkcja 2 ",
-        ".About": "O nas",
-        ".Login": "Login",
-        // Dodaj inne tłumaczenia dla polskiego tutaj
-      };
-      // Zmiana obrazu na "Palac.jpg"
-      $("#zdjecie-prawo").attr("src", "static/Palac.jpg");
-    } else if (language === "en") {
-      translations = {
-        "Zaloguj": "Login",
-        'label[for="username"]': "Username:",
-        'label[for="password"]': "Password:",
-        ".Dane_uzytkownika": "User Data",
-        ".Wersja_programu": "Program version" + programVersion,
-        ".Autorzy": "Authors Jakub Kołodziej i Piotr Wieczorek",
-        ".title": "Real estate price estimation",
-        ".pokoje-tekst": "Rooms:",
-        ".powierzchnia-tekst": "Area:",
-        ".prognozowana-cena": "Forecasted Price",
-        ".active": "Home",
-        ".Funkcja1": "Function 1",
-        ".Funkcja2": "Function 2 ",
-        ".About": "About us",
-        ".Login": "Login",
-        // Dodaj inne tłumaczenia dla angielskiego tutaj
-      };
-      // Zmiana obrazu na "BigBen.jpg"
-      $("#zdjecie-prawo").attr("src", "static/BigBen.jpg");
-    } else if (language === "spqr") {
-      translations = {
-        "Zaloguj": "Login",
-        'label[for="username"]': "Loginus:",
-        'label[for="password"]': "Password:",
-        ".Dane_uzytkownika": "User Data",
-        ".Wersja_programu": "Version program" + programVersion,
-        ".Autorzy": "Auctores Iacobus Wheelwright et Petrus Vesperi",
-        ".title": "Projecting verus praedium prices",
-        ".pokoje-tekst": "Rooms:",
-        ".powierzchnia-tekst": "Area:",
-        ".prognozowana-cena": "Data pretium",
-        ".active": "Home",
-        ".Funkcja1": "Officium 1",
-        ".Funkcja2": "Officium 2 ",
-        ".About": "De nobis",
-        ".Login": "Loginus",
-        // Dodaj inne tłumaczenia dla angielskiego tutaj
-      };
-      // Zmiana obrazu na "Roman Centurion.jpg"
-      $("#zdjecie-prawo").attr("src", "static/roman_centurion.jpg");
-    }
+// Funkcja do tłumaczenia elementów na podstawie mapy translations
+function translateElements(language) {
+  var translations = {};
 
-    for (var key in translations) {
-      if (translations.hasOwnProperty(key)) {
-        $(key).html(translations[key]);
-      }
-    }
-  }
-
-  // Ustaw flagę Polski jako domyślnie wybraną
-  $("#flag1").addClass("open");
-
-  // Wywołaj funkcję tłumaczącą elementy na język polski przy ładowaniu strony
-  translateElements("pl");
-
-  // Po kliknięciu na flagę
-  $('.flag-item').click(function() {
-    var clickedFlag = $(this);
-    var otherFlags = $('.flag-item').not(clickedFlag);
-
-    // Sprawdź, czy flaga jest już rozwinięta
-    if (clickedFlag.hasClass('open')) {
-      // Jeśli jest rozwinięta, zwiń ją
-      clickedFlag.removeClass('open');
-      otherFlags.slideDown(300); // Pokaż pozostałe flagi
-    } else {
-      // Jeśli nie jest rozwinięta, rozwij ją i ukryj pozostałe flagi
-      clickedFlag.addClass('open');
-      otherFlags.slideUp(300); // Zwiń pozostałe flagi
-    }
-
-    if (clickedFlag.attr("id") === "flag1") {
-      // Jeśli kliknięto flagę Polski, tłumacz na język polski
-      translateElements("pl");
-    } else if (clickedFlag.attr("id") === "flag2") {
-      // Jeśli kliknięto flagę UK, tłumacz na język angielski
-      translateElements("en");
-    } else if (clickedFlag.attr("id") === "flag3") {
-      // Jeśli kliknięto flagę SPQR, tłumacz na język SPQR
-      translateElements("spqr");
-    }
-  });
-
-  // Po zakończeniu animacji slideUp
-  $('.flag-item').on('slideUp', function() {
-    var flag = $(this);
-    if (!flag.hasClass('open')) {
-      flag.hide(); // Ukryj zwiniete flagi po zakończeniu animacji
-    }
-  });
-
-  // Po kliknięciu przycisku "Estymuj cenę"
-  $("#priceForm").submit(function(e) {
-    e.preventDefault(); // Zapobiega domyślnej akcji formularza (przeładowania strony)
-
-    // Dane z formularza
-    var formData = {
-      offer_type: $("#offer_type").val(),
-      area: parseFloat($("#area").val()),
-      rooms: parseInt($("#rooms").val()),
-      offer_type_of_building: $("#offer_type_of_building").val(),
-      market: $("#market").val(),
-      city_name: $("#city_name").val(),
-      voivodeship: $("#voivodeship").val(),
+  if (language === "pl") {
+    translations = {
+      // Tłumaczenia dla polskiego
     };
+    // Zmiana obrazu na "Palac.jpg"
+    $("#zdjecie-prawo").attr("src", "static/Palac.jpg");
+  } else if (language === "en") {
+    translations = {
+      // Tłumaczenia dla angielskiego
+    };
+    // Zmiana obrazu na "BigBen.jpg"
+    $("#zdjecie-prawo").attr("src", "static/BigBen.jpg");
+  } else if (language === "spqr") {
+    translations = {
+      // Tłumaczenia dla SPQR
+    };
+    // Zmiana obrazu na "Roman Centurion.jpg"
+    $("#zdjecie-prawo").attr("src", "static/roman_centurion.jpg");
+  }
 
-    // Wysłanie żądania POST do serwera
-    $.ajax({
-      type: "POST",
-      url: "/estimate_price",
-      data: formData,
-      success: function(data) {
-        // Aktualizacja pola "Estymowana cena" po otrzymaniu odpowiedzi od serwera
-        $("#estimatedPrice").text(data.estimated_price);
-      },
-      error: function() {
-        // Obsługa błędu
-        alert("Wystąpił błąd podczas estymacji ceny.");
-      },
-    });
+  for (var key in translations) {
+    if (translations.hasOwnProperty(key)) {
+      $(key).html(translations[key]);
+    }
+  }
+}
+
+// Ustaw flagę Polski jako domyślnie wybraną
+$("#flag1").addClass("open");
+
+// Wywołaj funkcję tłumaczącą elementy na język polski przy ładowaniu strony
+translateElements("pl");
+
+// Po kliknięciu na flagę
+$('.flag-item').click(function() {
+  var clickedFlag = $(this);
+  var otherFlags = $('.flag-item').not(clickedFlag);
+
+  // Sprawdź, czy flaga jest już rozwinięta
+  if (clickedFlag.hasClass('open')) {
+    // Jeśli jest rozwinięta, zwiń ją
+    clickedFlag.removeClass('open');
+    otherFlags.slideDown(300); // Pokaż pozostałe flagi
+  } else {
+    // Jeśli nie jest rozwinięta, rozwij ją i ukryj pozostałe flagi
+    clickedFlag.addClass('open');
+    otherFlags.slideUp(300); // Zwiń pozostałe flagi
+  }
+
+  if (clickedFlag.attr("id") === "flag1") {
+    // Jeśli kliknięto flagę Polski, tłumacz na język polski
+    translateElements("pl");
+  } else if (clickedFlag.attr("id") === "flag2") {
+    // Jeśli kliknięto flagę UK, tłumacz na język angielski
+    translateElements("en");
+  } else if (clickedFlag.attr("id") === "flag3") {
+    // Jeśli kliknięto flagę SPQR, tłumacz na język SPQR
+    translateElements("spqr");
+  }
+});
+
+// Po zakończeniu animacji slideUp
+$('.flag-item').on('slideUp', function() {
+  var flag = $(this);
+  if (!flag.hasClass('open')) {
+    flag.hide(); // Ukryj zwiniete flagi po zakończeniu animacji
+  }
+});
+
+// Po kliknięciu przycisku "Estymuj cenę"
+$("#priceForm").submit(function(e) {
+  e.preventDefault(); // Zapobiega domyślnej akcji formularza (przeładowania strony)
+
+  // Dane z formularza
+  var formData = {
+    offer_type: $("#offer_type").val(),
+    area: parseFloat($("#area").val()),
+    rooms: parseInt($("#rooms").val()),
+    offer_type_of_building: $("#offer_type_of_building").val(),
+    market: $("#market").val(),
+    city_name: $("#city_name").val(),
+    voivodeship: $("#voivodeship").val(),
+  };
+
+  // Wysłanie żądania POST do serwera
+  $.ajax({
+    type: "POST",
+    url: "/estimate_price",
+    data: formData,
+    success: function(data) {
+      // Aktualizacja pola "Estymowana cena" po otrzymaniu odpowiedzi od serwera
+      $("#estimatedPrice").text(data.estimated_price);
+
+      // Wysłanie danych do modelu Machine Learning na Azure
+      var azureFormData = {
+        "data": {
+          "offer_type": formData.offer_type,
+          "area": formData.area,
+          "rooms": formData.rooms,
+          "offer_type_of_building": formData.offer_type_of_building,
+          "market": formData.market,
+          "city_name": formData.city_name,
+          "voivodeship": formData.voivodeship
+        }
+      };
+
+      $.ajax({
+        type: "POST",
+        url: "http://382ac124-926d-4f00-b545-867d0ca18c9d.germanywestcentral.azurecontainer.io/score",
+        contentType: "application/json",
+        data: JSON.stringify(azureFormData),
+        success: function(result) {
+          // Aktualizacja wyniku prognozy od modelu
+          $("#azurePrediction").text(result.Results.WebServiceOutput0[0].predicted_price);
+        },
+        error: function() {
+          // Obsługa błędu
+          alert("Wystąpił błąd podczas komunikacji z modelem na Azure.");
+        }
+      });
+    },
+    error: function() {
+      // Obsługa błędu
+      alert("Wystąpił błąd podczas estymacji ceny.");
+    },
   });
+});
 
   // Obsługa zdarzenia kliknięcia na dokument
   $(document).click(function(event) {
